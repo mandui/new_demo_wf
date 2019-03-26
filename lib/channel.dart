3f9c36ddbe16e96066d3249519aed173b95efd06
onth3road <onth3road@sohu.com>
2019-03-26 20:22:20 +0800

    a

--- lib/channel.dart
+++ lib/channel.dart
@@ -23,7 +23,7 @@ class NewDemoChannel extends ApplicationChannel {
     router.route("/login/account/*").link(() => FileController("lib/pages/login/"));
     router.route("/login/id/*").link(() => FileController("lib/pages/login/"));
 
-    router.route("/entry/*").link(() => FileController("lib/pages/login/entry.html"));
+    router.route("/entry/*").link(() => FileController("lib/pages/login/"));
 
     router.route("/vote/*").link(() => FileController("lib/pages/vote/"));
     router.route("/account/*").link(() => FileController("lib/pages/account/"));
